2ceb07b178d22b8985371c5868058aad6ffc0982
GaikarChaitali <44640738+GaikarChaitali@users.noreply.github.com>
2018-11-19 08:26:52 +0530

    singletone

---
+++ TestShapes.java
@@ -0,0 +1,13 @@
+package CoreJavaAssignments;
+
+public class TestShapes {
+
+	public static void main(String[] args) {
+		Line line=new Line();
+		line.drawShape();
+		Rectangle rectangle=new Rectangle();
+		rectangle.drawShape();
+		Cube cube=new Cube();		
+		cube.drawShape();
+	}
+}
